1807797c84312e6c9b8645cdf48b7bbc4d907d7c
Quikler <romuchbinyk228@gmail.com>
2025-11-29 14:17:28 +0200

    feat: Only save files to temp directory

--- Models/DownloadResult.cs
+++ Models/DownloadResult.cs
@@ -2,18 +2,21 @@ namespace QuikytLoader.Models;
 
 /// <summary>
 /// Represents the result of a YouTube download operation
-/// Contains paths to both the audio file and optional thumbnail
+/// Contains paths to temporary files (both audio and thumbnail)
+/// These files are stored in temp directory and should be cleaned up after use
 /// </summary>
 public class DownloadResult
 {
     /// <summary>
-    /// Full path to the downloaded MP3 audio file
+    /// Full path to the temporary MP3 audio file
+    /// File is in temp directory and should be deleted after sending to Telegram
     /// </summary>
-    public required string AudioFilePath { get; init; }
+    public required string TempMediaFilePath { get; init; }
 
     /// <summary>
-    /// Full path to the downloaded thumbnail image (JPEG format)
+    /// Full path to the temporary thumbnail image (JPEG format)
     /// Null if thumbnail download failed or was not available
+    /// File is in temp directory and should be deleted after sending to Telegram
     /// </summary>
-    public string? ThumbnailPath { get; init; }
+    public string? TempThumbnailPath { get; init; }
 }
