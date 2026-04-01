40b458f3776e1457e98d5372fa2c4089b7614ad1
Quikler <149816244+Quikler@users.noreply.github.com>
2026-04-01 21:11:16 +0300

    Chore/Update to net10 (#13)

--- config.sh
+++ config.sh
@@ -1,9 +1,11 @@
 APP_NAME=QuikytLoader
-TFM=net9.0
+TFM=net10.0
+RID=linux-x64
+STARTUP_PROJ_NAME=$APP_NAME.Startup
 
 if [[ -z "$APP_NAME" ]]; then
   echo "Error: APP_NAME is empty or not set in config.sh. Aborting." >&2
   exit 1
 fi
 
-export APP_NAME TFM
+export APP_NAME TFM RID STARTUP_PROJ_NAME
