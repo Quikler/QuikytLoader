020d049eddc74086f76123afbdda6a7437174cbc
Quikler <romuchbinyk228@gmail.com>
2026-07-22 14:04:35 +0200

    Remove ObservableObject inheritance from DownloadQueueManager

--- QuikytLoader.AvaloniaUI/Services/DownloadQueueManager.cs
+++ QuikytLoader.AvaloniaUI/Services/DownloadQueueManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using CommunityToolkit.Mvvm.ComponentModel;
 using QuikytLoader.Application.Interfaces.Queue;
 using QuikytLoader.Application.UseCases;
 using QuikytLoader.AvaloniaUI.ViewModels.Queue.QueueEntry;
@@ -10,7 +9,7 @@ using QuikytLoader.Domain.Entities;
 
 namespace QuikytLoader.AvaloniaUI.Services;
 
-public partial class DownloadQueueManager : ObservableObject
+public class DownloadQueueManager
 {
     private readonly IDownloadQueue _queue;
     private readonly IDownloadQueueProcessor _queueProcessor;
